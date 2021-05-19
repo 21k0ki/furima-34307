7358d2bca32b213b9551db32d55adc180264eb8e
21k0ki <gogo.21koki@gmail.com>
2021-05-19 19:34:41 +0900

    ユーザーとアイテムのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ApplicationRecord
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/	}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/	}
     validates :birthday
+    has_many :items
   end
 end
