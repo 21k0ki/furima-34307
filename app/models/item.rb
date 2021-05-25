1b05c16c00ac52fb3854411f77a986d39d85a819
21k0ki <gogo.21koki@gmail.com>
2021-05-25 22:49:44 +0900

    テストコード等の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,30 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belonfgs_to :category
+
+  belongs_to :category
   belongs_to :item_status
   belongs_to :shopping_charge
   belongs_to :shopping_area
   belongs_to :estimated_shopping_date
+  
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :item_description
-    validates :category_id
-    validates :item_status_id
-    validates :shopping_charge_id
-    validates :shopping_area_id
-    validates :estimated_shopping_date_id
-    validates :selling_price
+    validates :selling_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}, format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 1 } do
+  validates :category_id
+  validates :item_status_id
+  validates :shopping_charge_id
+  validates :shopping_area_id
+  validates :estimated_shopping_date_id
+  end
 end
+
+
+# numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}}
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+   context'商品の出品ができないとき' do 
+
+    it 'imageが１枚ないと登録できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'item_nameがないと登録できない' do
+      @item.item_name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    end
+    it 'item_descriptionがないと登録できない' do
+      @item.item_description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item description can't be blank")
+    end
+    it 'category_idがないと登録でいない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+    it 'item_status_idがないと登録できない'do
+      @item.item_status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item status must be other than 1")
+    end
+    it 'shopping_charge_idがないと登録できない' do
+      @item.shopping_charge_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shopping charge must be other than 1")
+    end
+    it 'shopping_area_idがないと登録できない' do
+      @item.shopping_area_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shopping area must be other than 1")
+    end
+    it 'estimated_shopping_date_idがないと登録できない' do
+      @item.estimated_shopping_date_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Estimated shopping date must be other than 1")
+    end
+    it 'selling_priceがないと登録できない' do
+      @item.selling_price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Selling price can't be blank")
+    end
+    it 'selling_priceが299円だと登録できない' do
+      @item.selling_price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Selling price is invalid")
+    end
+    it 'selling_priceが10000000だと登録できない' do
+      @item.selling_price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Selling price is invalid")
+    end
+    it 'selling_priceが半角英数でないと出品できない' do
+      @item.selling_price = "100aaa"
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Selling price is invalid")
+    end
+
+
+    context'商品の出品ができるとき' do
+     it'image,item_name,item_description,category,item_status,shopping_charge,shopping_area,estimated_shopping_date,selling_priceが存在するとき' do
+      expect(@item).to be_valid
+     end
+    end
+
+    end
+  end
 end
+
+
