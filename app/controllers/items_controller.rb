0868c91cad6cd5f504e44fbfc17f48166d48184f
21k0ki <gogo.21koki@gmail.com>
2021-06-09 21:14:30 +0900

    詳細関連

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,10 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
 
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_status_id, :shopping_charge_id, :shopping_area_id, :estimated_shopping_date_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 
 end
