aca14dcbf815e2826f884d22cad0cc42ff3a75e4
21k0ki <gogo.21koki@gmail.com>
2021-06-09 23:08:30 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
