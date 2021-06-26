81e6791fe1e094ccbf64aa879e1257b40a0203d3
21k0ki <gogo.21koki@gmail.com>
2021-06-26 15:30:18 +0900

    条件分岐変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:update, :destroy, :edit]
 
@@ -49,9 +49,10 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if @item.purchase.present? || @item.user_id
+    if @item.user_id != current_user.id || @item.purchase.present?
       redirect_to root_path
     end
   end
 
 end
+
