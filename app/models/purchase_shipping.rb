5a2703e5f8a8f95b5050786b121e4fa6d1c46056
21k0ki <gogo.21koki@gmail.com>
2021-06-28 21:57:14 +0900

    Update purchase_shipping.rb

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,6 +1,6 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token, :purchase_id
 
   with_options presence: true do
   validates :item_id
@@ -15,6 +15,6 @@ class PurchaseShipping
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
